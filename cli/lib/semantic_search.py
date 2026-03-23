e4e573a45bc1c70f002d3984fbe133c5b6c51ab1
dejworj <dejworj@shure.com>
2026-03-23 16:31:04 -0500

    testing model verify

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -5,11 +5,7 @@ class SemanticSearch:
 
     def __init__(self):
         self.model = SentenceTransformer("all-MiniLM-L6-v2")
-        self.max_sequence_length = 0
-
-    def verify(self):
-        """Verify the model information"""
-        verify_model()
+        self.max_sequence_length = 256
 
     
 def verify_model():
